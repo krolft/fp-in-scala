e3de52021a15886413e3759af752f077923fe89b
krolft <jeheim@posteo.de>
2018-06-17 23:09:00 +0200

    Redo chapter 7

--- src/main/scala-2.12/chapter07/parallel.scala
+++ src/main/scala-2.12/chapter07/parallel.scala
@@ -1,93 +1,12 @@
 package chapter07
 
-import java.util.concurrent.TimeUnit
-
-object parallel {
-
-  import Par._
-
-  def sum(ints: IndexedSeq[Int]): Int =
-    if (ints.size <= 1)
-      ints.headOption getOrElse 0
-    else {
-      val (l, r) = ints.splitAt(ints.size / 2)
-      sum(l) + sum(r)
-    }
-
-  //  def sumWithParType(ints: IndexedSeq[Int]): Int =
-  //    if (ints.size <= 1)
-  //      ints.headOption getOrElse 0
-  //    else {
-  //      val (l, r) = ints.splitAt(ints.length / 2)
-  //      val sumL: Par[Int] = unit(sumWithParType(l))
-  //      val sumR: Par[Int] = unit(sumWithParType(r))
-  //      val es = new ExecutorService
-  //      run(es)(sumL) + run(es)(sumR)
-  //    }
-
-  def sumDeferringCallToGet(ints: IndexedSeq[Int]): Par[Int] =
-    if (ints.size <= 1)
-      unit(ints.headOption getOrElse 0)
-    else {
-      val (l, r) = ints.splitAt(ints.length / 2)
-      map2(sumDeferringCallToGet(l), sumDeferringCallToGet(r))(_ + _)
-    }
-
-  def sumWithFork(ints: IndexedSeq[Int]): Par[Int] =
-    if (ints.size <= 1)
-      lazyUnit(ints.headOption getOrElse 0)
-    else {
-      val (l, r) = ints.splitAt(ints.length / 2)
-      map2(fork(sumWithFork(l)), fork(sumWithFork(r)))(_ + _)
-    }
-
-  def max(ints: IndexedSeq[Int]): Par[Option[Int]] =
-    if (ints.size <= 1)
-      lazyUnit(ints.headOption)
-    else {
-      val (l, r) = ints.splitAt(ints.length / 2)
-      map2(fork(max(l)), fork(max(r)))(comb(_, _)(math.max))
-    }
-
-  def comb[A](optionA: Option[A], optionB: Option[A])(f: (A, A) => A): Option[A] =
-    (optionA, optionB) match {
-      case (None, Some(b)) => Some(b)
-      case (Some(a), None) => Some(a)
-      case (Some(a), Some(b)) => Some(f(a, b))
-      case _ => None
-    }
-
-  def words(lines: List[String]): Par[Int] = {
-    // sequential approach
-    // lines.foldLeft(List[Int]())((list, line) => line.length :: list).sum
-
-    // what could be done in parallel
-    // a) calculating the length of lines
-    // b) adding up the lengths of lines
-
-    val parOfLineLengths: Par[List[Int]] = parMap(lines)(line => line.length)
-
-    // the following line wouldn't sum in parallel
-    // map(parOfLineLengths)(list => list.sum)
-
-    foldLeft(parOfLineLengths)(0)(_ + _)
-
-    // an alternative would be:
-    // foldLeft(unit(lines))(0)(_.length + _)
-  }
-
-  def sortPar(parList: Par[List[Int]]): Par[List[Int]] =
-    map2(parList, unit(()))((list, _) => list.sorted)
-
-  def sortParWithMap(parList: Par[List[Int]]): Par[List[Int]] =
-    map(parList)(_.sorted)
-}
+import scala.concurrent.duration.TimeUnit
 
 class ExecutorService {
   def submit[A](a: Callable[A]): Future[A] = ???
 }
+
 trait Callable[A] {
-  // just to be able to retrieve an A in a lazy manner
   def call: A
 }
 
@@ -103,8 +22,8 @@ trait Future[A] {
   def isCancelled: Boolean
 }
 
-case class UnitFuture[A](a: A) extends Future[A] {
-  override def get: A = a
+private case class UnitFuture[A](value: A) extends Future[A] {
+  override def get: A = value
 
   override def get(timeout: Long, unit: TimeUnit): A = get
 
@@ -116,68 +35,114 @@ case class UnitFuture[A](a: A) extends Future[A] {
 }
 
 object Par {
-
   type Par[A] = ExecutorService => Future[A]
 
   def unit[A](a: A): Par[A] = _ => UnitFuture(a)
 
-  def fork[A](a: => Par[A]): Par[A] = es => {
+  def fork[A](a: Par[A]): Par[A] = es =>
     es.submit(new Callable[A] {
       override def call: A = a(es).get
     })
-  }
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
-  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+  def run[A](es: ExecutorService)(a: Par[A]): Future[A] = a(es)
 
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = es => {
-    val af = a(es)
-    val bf = b(es)
-    UnitFuture(f(af.get, bf.get))
+  def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] = es => {
+    val a = pa(es).get
+    val b = pb(es).get
+    UnitFuture(f(a, b))
   }
+
+  def asyncF[A, B](f: A => B): A => Par[B] =
+    a => lazyUnit(f(a))
+
+  def sortParCustom(pas: Par[List[Int]]): Par[List[Int]] =
+    map2(pas, unit(()))((list, _) => list.sorted)
 
   def map[A, B](pa: Par[A])(f: A => B): Par[B] =
     map2(pa, unit(()))((a, _) => f(a))
 
-  def sequence[A](pas: List[Par[A]]): Par[List[A]] =
-    pas.foldRight[Par[List[A]]](unit(List()))((h, t) => map2(h, t)(_ :: _))
+  def sortPar(pas: Par[List[Int]]): Par[List[Int]] =
+    map(pas)(_.sorted)
 
-  def foldLeft[A, B](pas: Par[List[A]])(z: B)(f: (A, B) => B): Par[B] = {
-    // TODO ... use eval async and flatMap
-    map(pas)(as => as.reduceLeftOption((acc, a) => f(a, acc)).getOrElse(z))
+  def sequence[A](lpa: List[Par[A]]): Par[List[A]] =
+    lpa.foldRight[Par[List[A]]](unit(List()))((pa, pla) => map2(pa, pla)(_ :: _))
+
+  def parMap[A, B](as: List[A])(f: A => B): Par[List[B]] = fork {
+    val pbs: List[Par[B]] = as.map(asyncF(f(_)))
+    sequence(pbs)
   }
 
-  def parMap[A, B](pas: List[A])(f: A => B): Par[List[B]] = {
-    val list = pas.map(evalAsync(f))
-    sequence(list)
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = fork {
+    as.foldRight(unit(List[A]()))(
+      (a, pas) =>
+        map2(asyncF(f)(a), pas) {
+          (result, as) => if (result) a :: as else as
+        }
+    )
   }
 
-  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
-    // the following is not doing it in parallel but only later in one parallel thread
-    // lazyUnit(as.filter(f))
-
-    // we want to use evalAsync for EVERY element of the list
-    val listOfParOfListOfA: List[Par[List[A]]] = as.map(evalAsync[A, List[A]](a => if (f(a)) List[A](a) else List[A]()))
-
-    // we want to get from  List[Par[List[A]]] to Par[List[A]]
-    // Par[List[List[A]]] is also ok, we can flatten it
-    val parOfListOfListA: Par[List[List[A]]] = sequence(listOfParOfListOfA)
-
-    // now we can map over Par using our map to access the list of lists and flatten it
-    val parOfListOfA: Par[List[A]] = map(parOfListOfListA)(listOfListA => listOfListA.flatten)
-
-    // we are done!
-    parOfListOfA
+  def parFilterFromBook[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
+    val pas = as.map(asyncF((a: A) => if (f(a)) a :: Nil else Nil)(_))
+    map(sequence(pas))(_.flatten)
   }
 
-  def evalAsyncCustom[A, B](f: A => B): A => Par[B] =
-    a => es =>
-      es.submit(new Callable[B] {
-        override def call: B = f(a)
-      })
+  def sum(ints: IndexedSeq[Int]): Par[Int] =
+    if (ints.length <= 1)
+      unit(ints.headOption getOrElse 0)
+    else {
+      val (a, b) = ints.splitAt(ints.length / 2)
+      map2(fork(sum(a)), fork(sum(b)))(_ + _)
+    }
 
-  def evalAsync[A, B](f: A => B): A => Par[B] =
-    a => lazyUnit(f(a))
+  def max(ints: IndexedSeq[Int]): Par[Int] =
+    if (ints.length <= 1)
+      unit(ints.headOption getOrElse Int.MinValue)
+    else {
+      val (a, b) = ints.splitAt(ints.length / 2)
+      map2(fork(sum(a)), fork(sum(b)))(Math.max)
+    }
 
+  def reduce[A](as: IndexedSeq[A], zero: A)(f: (A, A) => A): Par[A] =
+    if (as.length <= 1)
+      unit(as.headOption getOrElse zero)
+    else {
+      val (a, b) = as.splitAt(as.length / 2)
+      map2(fork(reduce(a, zero)(f)), fork(reduce(b, zero)(f)))(f)
+    }
+
+  def maxUsingReduce(ints: IndexedSeq[Int]): Par[Int] =
+    reduce(ints, Int.MinValue)(Math.max)
+
+  def sumUsingReduce(ints: IndexedSeq[Int]): Par[Int] =
+    reduce(ints, 0)(_ + _)
+
+  def countWords(lines: IndexedSeq[String]): Par[Int] =
+    if (lines.length <= 1)
+      unit(lines.headOption.map(_.split("\\s+").length) getOrElse 0)
+    else {
+      val (a, b) = lines.splitAt(lines.length / 2)
+      map2(fork(countWords(a)), fork(countWords(b)))(_ + _)
+    }
+
+  def mapReduce[A, B](as: IndexedSeq[A], zero: B)(m: A => B)(r: (B, B) => B): Par[B] =
+    if (as.length <= 1)
+      unit(as.headOption.map(m) getOrElse zero)
+    else {
+      val (a, b) = as.splitAt(as.length / 2)
+      map2(fork(mapReduce(a, zero)(m)(r)), fork(mapReduce(b, zero)(m)(r)))(r)
+    }
+
+  def countWordsUsingMapReduce(lines: IndexedSeq[String]): Par[Int] =
+    mapReduce(lines, 0)(_.split("\\s+").length)(_ + _)
+
+  def map3[A, B, C, D](pa: Par[A], pb: Par[B], pc: Par[C])(f: (A, B, C) => D): Par[D] = {
+
+    def curry[E, F, G, H](j: (E, F, G) => H): (E, F) => G => H =
+      (e, f) => g => j(e, f, g)
+
+    val parOfCToD: Par[C => D] = map2[A, B, C => D](pa, pb)((a, b) => curry(f)(a, b))
+    map2(pc, parOfCToD)((c, cToD) => cToD(c))
+  }
 }
--- src/test/scala-2.12/chapter07/ParallelTest.scala
+++ src/test/scala-2.12/chapter07/ParallelTest.scala
@@ -2,8 +2,13 @@ package chapter07
 
 import org.scalatest.{FlatSpec, Matchers}
 
-class ParallelTest extends FlatSpec with Matchers {
-  "Summing in parallel" should "still work" in {
-    parallel.sum(Vector(1, 2, 3, 4, 5)) shouldBe 15
+class Parallel2Test extends FlatSpec with Matchers {
+  "Fold left and fold right" should "join lists as expected" in {
+    // fold left is starting from left
+    List(1, 2, 3).foldLeft(List[Int]())((acc, x) => x :: acc) shouldBe List(3, 2, 1)
+    List(1, 2, 3).foldLeft(List[Int]())((acc, x) => acc :+ x) shouldBe List(1, 2, 3)
+
+    // fold right is starting from right
+    List(1, 2, 3).foldRight(List[Int]())((x, acc) => x :: acc) shouldBe List(1, 2, 3)
   }
 }
